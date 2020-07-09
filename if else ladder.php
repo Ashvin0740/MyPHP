5784aa83189e9e58bd42e9e5ae41de6b38c03928
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2020-07-09 17:23:53 +0530

    if else laadder

---
+++ if else ladder.php
@@ -0,0 +1,18 @@
+<?php
+$age = 18;
+
+if($age > 19 )
+{
+ echo " Are you able to voting ";
+}
+else if($age >10) 
+{
+    echo"Are you  not able to voting";
+}
+else 
+{
+    echo " you atr chiled.";
+}
+
+
+?>
